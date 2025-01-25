5fe1f288ca08fe79f7cc899c66e3ad805e3e40ee
Wimpy57 <venigretdj@gmail.com>
2025-01-25 17:21:38 +0300

    MainMenu
    Completed navigation

--- Assets/Core/Scripts/MainMenu/ButtonLogic.cs
+++ Assets/Core/Scripts/MainMenu/ButtonLogic.cs
@@ -6,25 +6,35 @@ using UnityEngine;
 public class ButtonLogic : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI[] buttonsText;
 
+    private Color _defaultColor;
+    private Color _selectedColor;
     private int _selected = 0;
 
     private void Start() {
         buttonsText[_selected].text = ">" + buttonsText[_selected].text + "<";
+        buttonsText[_selected].color = Color.black;
+        _defaultColor = buttonsText[_selected+1].color;
+        _selectedColor = buttonsText[_selected].color;
     }
     
     private void Update() {
         if (Input.GetKeyDown(KeyCode.DownArrow) && _selected < buttonsText.Length - 1) {
             buttonsText[_selected].text = buttonsText[_selected]
                 .text.Substring(1, buttonsText[_selected].text.Length - 2);
+            buttonsText[_selected].color = _defaultColor;
             _selected++;
             buttonsText[_selected].text = ">" + buttonsText[_selected].text + "<";
+            buttonsText[_selected].color = _selectedColor;
+
         }
 
         if (Input.GetKeyDown(KeyCode.UpArrow) && _selected > 0) {
             buttonsText[_selected].text = buttonsText[_selected]
                 .text.Substring(1, buttonsText[_selected].text.Length - 2);
+            buttonsText[_selected].color = _defaultColor;
             _selected--;
             buttonsText[_selected].text = ">" + buttonsText[_selected].text + "<";
+            buttonsText[_selected].color = _selectedColor;
         }
 
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
