84b4d6ab9ad43c7f5c0044f69b5aac2a0433c5b9
semapaizuri <73467688+semapaizuri@users.noreply.github.com>
2025-01-26 05:20:48 +0300

    anims

--- Assets/Core/Scripts/Player.cs
+++ Assets/Core/Scripts/Player.cs
@@ -8,50 +8,86 @@ public class Player : MonoBehaviour {
     
     public event EventHandler<OnDoorOpenEventArgs> OnDoorOpened;
 
+    
+
     public class OnDoorOpenEventArgs : EventArgs {
         public Transform senderTransform;
     }
 
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Animator _anim;
 
-    
+    private Rigidbody2D _rb;
     private InteractiveObject _selectedInteractiveObject;
     private bool _isWalking;
+    private Vector2 _inputVector;
+    private float _x;
+    private float _y;
 
     private void Awake() {
-        Instance = this; 
+        Instance = this;
     }
 
     private void Start() {
         GameInput.Instance.OnInteractAction += GameInput_OnInteractAction;
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    private void GameInput_OnInteractAction(object sender, EventArgs e) {
-        if (_selectedInteractiveObject != null) {
-            _selectedInteractiveObject.Interact();
-            if (_selectedInteractiveObject is Door) {
-                OnDoorOpened?.Invoke(this, new OnDoorOpenEventArgs {
-                        senderTransform = _selectedInteractiveObject.transform
-                    }
-                );
-            }
-        }
+    private void Update()
+    {
+        GetMovementInput();
+        Animate();
     }
 
-    private void Update() {
-        HandleMovement();
+    private void FixedUpdate() {
+        Movement();
     }
 
-    private void HandleMovement() {
-        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
-        Vector3 moveDirection = new Vector3(inputVector.x, inputVector.y, 0);
+    private void Movement() 
+    {
+        _rb.linearVelocity = _inputVector * moveSpeed;
+    }
 
-        if (moveDirection != Vector3.zero) {
-            transform.position += moveDirection * (moveSpeed * Time.deltaTime);
+    private void GetMovementInput()
+    {
+        _x = Input.GetAxisRaw("Horizontal");
+        _y = Input.GetAxisRaw("Vertical");
+        _inputVector = new Vector2(_x, _y);
+        _inputVector.Normalize();
+    }
+
+    private void Animate()
+    {
+        if (_inputVector.magnitude != 0f)
+        {
             _isWalking = true;
         }
-        else {
+        else
+        {
             _isWalking = false;
+        }
+
+        if (_isWalking)
+        {
+            _anim.SetFloat("x", _x);
+            _anim.SetFloat("y", _y);
+        }
+        _anim.SetBool("moving", _isWalking);
+    }
+
+    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    {
+        if (_selectedInteractiveObject != null)
+        {
+            _selectedInteractiveObject.Interact();
+            if (_selectedInteractiveObject is Door)
+            {
+                OnDoorOpened?.Invoke(this, new OnDoorOpenEventArgs
+                {
+                    senderTransform = _selectedInteractiveObject.transform
+                }
+                );
+            }
         }
     }
 
