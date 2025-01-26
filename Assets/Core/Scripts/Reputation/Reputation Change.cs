096252d862565243e253c3afbd3ca0806992e8aa
quq-quq <kubrakfedor@gmail.com>
2025-01-26 19:05:26 +0300

    19:05

--- Assets/Core/Scripts/Reputation/Reputation Change.cs
+++ Assets/Core/Scripts/Reputation/Reputation Change.cs
@@ -9,6 +9,7 @@ public class ReputationChange : MonoBehaviour
     
     [SerializeField, Range(0, 1)] private float _startReputation;
     [SerializeField, Range(0, 1)] private float _whenPassed;
+    [SerializeField] private bool _ifZeroViewers = false;
     [SerializeField] private Scrollbar _scrollbar;
 
     private void Start()
@@ -18,19 +19,24 @@ public class ReputationChange : MonoBehaviour
 
     private void Update()
     {
+        if (_ifZeroViewers)
+            return;
+
         DialogueViewer[] dialogues = FindObjectsOfType<DialogueViewer>();
 
+        Debug.Log(dialogues.Length);
         if (dialogues.Length == 0)
         {
+            _ifZeroViewers=true;
             if(_scrollbar.value >= _whenPassed)
             {
                 ConfigGameManager.Instance.ExamPassed(SceneManager.GetActiveScene().name);
+                Debug.Log("Complete");
             }
             else if (_scrollbar.value < _whenPassed) {
                 ConfigGameManager.Instance.ExamFailed();
+                Debug.Log("fail");
             }
-            
-            Destroy(gameObject);
         }
     }
 
