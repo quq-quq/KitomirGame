3a6e0d7d7e4660f551dd12aba9ce326c28bc83b4
Wimpy57 <venigretdj@gmail.com>
2025-02-15 15:46:58 +0300

    MainMenu
    Added main menu navigation

--- Assets/Core/Scripts/Managers/InputManager.cs
+++ Assets/Core/Scripts/Managers/InputManager.cs
@@ -1,14 +1,19 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
-    
+
 public class InputManager : MonoBehaviour
 {
     public static InputManager Instance { get; private set; }
     public event EventHandler OnInteractAction;
+    public event EventHandler OnMainMenuSwitchUp;
+    public event EventHandler OnMainMenuSwitchDown;
+    public event EventHandler OnMainMenuButtonPressed;
     
     private PlayerInputActions _playerInputActions;
+    private MainMenuInputActions _menuInputActions;
     
     private void Awake()
     {
@@ -24,6 +29,30 @@ public class InputManager : MonoBehaviour
         _playerInputActions = new PlayerInputActions();
         _playerInputActions.Player.Enable();
         _playerInputActions.Player.Interact.performed += Interact_performed;
+
+        if (SceneManager.GetActiveScene().name == SceneNames.MAIN_MENU_SCENE)
+        {
+            _menuInputActions = new MainMenuInputActions();
+            _menuInputActions.Enable();
+            _menuInputActions.Navigation.UpSwitch.performed += UpSwitch_performed;
+            _menuInputActions.Navigation.DownSwitch.performed += DownSwitch_performed;
+            _menuInputActions.Navigation.PressButton.performed += PressButton_performed;
+        }
+    }
+
+    private void PressButton_performed(InputAction.CallbackContext obj)
+    {
+        OnMainMenuButtonPressed?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void DownSwitch_performed(InputAction.CallbackContext obj)
+    {
+        OnMainMenuSwitchDown?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void UpSwitch_performed(InputAction.CallbackContext obj)
+    {
+        OnMainMenuSwitchUp?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetMovementVectorNormalised()
@@ -42,5 +71,13 @@ public class InputManager : MonoBehaviour
     {
         _playerInputActions.Player.Interact.performed -= Interact_performed;
         _playerInputActions.Player.Disable();
+
+        if (SceneManager.GetActiveScene().name == SceneNames.MAIN_MENU_SCENE)
+        {
+            _menuInputActions.Navigation.UpSwitch.performed -= UpSwitch_performed;
+            _menuInputActions.Navigation.DownSwitch.performed -= DownSwitch_performed;
+            _menuInputActions.Navigation.PressButton.performed -= PressButton_performed;
+            _menuInputActions.Disable();
+        }
     }
 }
---
+++ Assets/Core/Scripts/UI/ButtonContainer.cs
@@ -0,0 +1,73 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class ButtonContainer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI[] _buttons;
+
+    private readonly Color _selectedColorText = Color.black;
+    private readonly Color _unselectedColorText = new (0.490566f, 0.490566f, 0.490566f, 1f);
+    
+    private int _selectedButtonId;
+
+    private void Start()
+    {
+        _selectedButtonId = 0;
+
+        for (int i = 0; i < _buttons.Length; i++)
+        {
+            _buttons[i].color = _unselectedColorText;
+        }
+        
+        SelectButton(_selectedButtonId);
+    
+        InputManager.Instance.OnMainMenuSwitchDown += InputManager_MainMenuSwitchDown;
+        InputManager.Instance.OnMainMenuSwitchUp += InputManager_MainMenuSwitchUp;
+        InputManager.Instance.OnMainMenuButtonPressed += InputManager_MainMenuButtonPressed;
+    }
+
+    private void InputManager_MainMenuButtonPressed(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void InputManager_MainMenuSwitchUp(object sender, EventArgs e)
+    {
+        if (_selectedButtonId > 0)
+        {
+            DeselectButton(_selectedButtonId);
+            _selectedButtonId--;
+            SelectButton(_selectedButtonId);
+        }
+    }
+
+    private void InputManager_MainMenuSwitchDown(object sender, EventArgs e)
+    {
+        if (_selectedButtonId < _buttons.Length - 1)
+        {
+            DeselectButton(_selectedButtonId);
+            _selectedButtonId++;
+            SelectButton(_selectedButtonId);
+        }
+    }
+
+    private void SelectButton(int buttonId)
+    {
+        _buttons[buttonId].color = _selectedColorText;
+        _buttons[buttonId].text = ">" + _buttons[buttonId].text + "<";
+    }
+
+    private void DeselectButton(int buttonId)
+    {
+        _buttons[buttonId].color = _unselectedColorText;
+        _buttons[buttonId].text = _buttons[buttonId].text.Substring(1, _buttons[buttonId].text.Length - 2);
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.OnMainMenuSwitchDown -= InputManager_MainMenuSwitchDown;
+        InputManager.Instance.OnMainMenuSwitchUp -= InputManager_MainMenuSwitchUp;
+        InputManager.Instance.OnMainMenuButtonPressed -= InputManager_MainMenuButtonPressed;
+    }
+}
